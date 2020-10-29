40c8d1ba492bb2de065aabeab68db2a1bee766e4
William <william.plucknett@uky.edu>
2020-10-29 14:20:43 -0400

    Resolved some issues with init.sql regarding the foreign keys. The tables now properly generate, but temperature does not reference .uid=1000(smartsparks) gid=1000(smartsparks) groups=1000(smartsparks),4(adm),24(cdrom),27(sudo),30(dip),46(plugdev),118(lxd),127(lpadmin),134(sambashare) right now.

--- home-server/backend/init.sql
+++ home-server/backend/init.sql
@@ -1,14 +1,15 @@
-DROP DATABASE IF EXISTS `hems`;
+#DROP DATABASE IF EXISTS `hems`;
 CREATE DATABASE IF NOT EXISTS hems;
 USE hems;
 CREATE TABLE IF NOT EXISTS `hems`.`devices`(
     id INT NOT NULL auto_increment, #Device ID
     name VARCHAR(50),               #Device Name
     last_update DATETIME,           #Last Time Device was touched
+    plug BOOL,                      #true if the device is a plug, false if the device is a tempsensor
     status BOOL,                    #Whether device is online or not
     PRIMARY KEY (id)                #Index on Device ID
 ) ENGINE=InnoDB;
-CREATE TABLE IF NOT EXISTS hems.`data`(
+CREATE TABLE IF NOT EXISTS `hems`.`energy`(
     id INT NOT NULL,                #Device ID
     time DATETIME NOT NULL,                  #Time of submission
     irms DOUBLE NOT NULL,                    #RMS current for datapoint
@@ -21,4 +22,13 @@ CREATE TABLE IF NOT EXISTS hems.`data`(
         ON DELETE CASCADE
         ON UPDATE RESTRICT
 ) ENGINE=InnoDB;
-
+CREATE TABLE IF NOT EXISTS `hems`.`temperature`(
+    id INT NOT NULL,
+    time DATETIME NOT NULL,
+    temperature DOUBLE NOT NULL,
+    primary key(time)
+#    CONSTRAINT fk_id FOREIGN KEY (id)
+#        REFERENCES hems.`devices`(id)
+#        ON DELETE CASCADE
+#        ON UPDATE RESTRICT
+) ENGINE=InnoDB;
