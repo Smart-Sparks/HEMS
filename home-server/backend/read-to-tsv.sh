3e3bde95806427ae696c8a040aa63979cd46c8de
William <william.plucknett@uky.edu>
2020-11-02 21:40:41 -0500

    fixed read-to-tsv.sh

--- home-server/backend/read-to-tsv.sh
+++ home-server/backend/read-to-tsv.sh
@@ -1,6 +1,6 @@
 # send to /homeBackend/input
 #datetime=`date +"%D %T"`
-PATHNAME="~/homeBackend/input/"
-`echo 'SELECT * FROM devices' | sudo mariadb -B hems` > "${PATHNAME}devices.tsv"
-`echo 'SELECT * FROM energy' | sudo mariadb -B hems` > "${PATHNAME}energy.tsv"
-`echo 'SELECT * FROM temperature' | sudo mariadb -B hems` > "${PATHNAME}temperature.tsv"
+PATHNAME=~/homeBackend/input/
+( echo 'SELECT * FROM devices' | sudo mariadb -B hems ) > ${PATHNAME}devices.tsv
+( echo 'SELECT * FROM energy' | sudo mariadb -B hems ) > ${PATHNAME}energy.tsv
+( echo 'SELECT * FROM temperature' | sudo mariadb -B hems ) > ${PATHNAME}temperature.tsv
