0b9db28618f3d219d1ada5105abfd73d8f65f67b
William <william.plucknett@uky.edu>
2020-11-02 22:30:16 -0500

    updated read-to-tsv.sh to use the date -R of when the script started as a prefix for the tsvs for the central server

--- home-server/backend/read-to-tsv.sh
+++ home-server/backend/read-to-tsv.sh
@@ -1,6 +1,9 @@
 # send to /homeBackend/input
-#datetime=`date +"%D %T"`
+# datetime=`date +"%D%T" -R`
+datetime=`date -R | tr -d :,./ | tr -d [:blank:]`
+fileprefix=${datetime:3:-5}
 PATHNAME=~/homeBackend/input/
-( echo 'SELECT * FROM devices' | sudo mariadb -B hems ) > ${PATHNAME}devices.tsv
-( echo 'SELECT * FROM energy' | sudo mariadb -B hems ) > ${PATHNAME}energy.tsv
-( echo 'SELECT * FROM temperature' | sudo mariadb -B hems ) > ${PATHNAME}temperature.tsv
+echo ${PATHNAME}${fileprefix}devices.tsv
+( echo 'SELECT * FROM devices' | sudo mariadb -B hems ) > ${PATHNAME}${fileprefix}devices.tsv
+( echo 'SELECT * FROM energy' | sudo mariadb -B hems ) > ${PATHNAME}${fileprefix}energy.tsv
+( echo 'SELECT * FROM temperature' | sudo mariadb -B hems ) > ${PATHNAME}${fileprefix}temperature.tsv
---
+++ home-server/backend/write-all.sh
@@ -0,0 +1,3 @@
+# call all of the python scripts that write to the tsv files
+python3 write-energy.py
+python3 write-temperature.py
