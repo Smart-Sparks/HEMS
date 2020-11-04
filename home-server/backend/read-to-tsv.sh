5ac568fd72f10093bd56399b545661615b30ba2d
William <william.plucknett@uky.edu>
2020-11-04 16:05:27 -0500

    changes to read-to-tsv.sh

--- home-server/backend/read-to-tsv.sh
+++ home-server/backend/read-to-tsv.sh
@@ -1,3 +1,7 @@
+# William Plucknett
+# Pull all of the data out of the hems database and write them in
+# separate .tsv files for Chase's central server to pick up.
+
 # send to /homeBackend/input
 # datetime=`date +"%D%T" -R`
 datetime=`date -R | tr -d :,./ | tr -d [:blank:]`
