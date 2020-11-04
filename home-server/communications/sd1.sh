8e7a664ee17e3a5a4f8cd79d8f10699e675fb987
Brooke <SmartSparksUKY@gmail.com>
2020-11-04 14:05:36 -0500

    updates for integration sd1

---
+++ home-server/communications/sd1.sh
@@ -0,0 +1,8 @@
+bash connectsd1.sh
+sleep 5
+echo 1 $(date '+%Y-%m-%d %H:%M:%S') > SmartDevice1.txt
+sudo echo a >> /dev/rfcomm0 2>&1
+sleep 1
+sudo cat /dev/rfcomm0 >> SmartDevice1.txt 2>&1
+sleep 15
+bash disconnect.sh 
