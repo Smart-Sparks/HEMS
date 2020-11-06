b8660156b9c95d748df4034760aa9377b51f0b4d
William <william.plucknett@uky.edu>
2020-11-06 16:44:24 -0500

    should be able to check for previous upload time

--- home-server/backend/read-to-tsv.sh
+++ home-server/backend/read-to-tsv.sh
@@ -8,7 +8,10 @@
 datetime=`date -R | tr -d :,./ | tr -d [:blank:]`
 fileaffix=${datetime:3:-5}
 PATHNAME=~/homeBackend/input/
+prevupload="prevdatetime.txt"
+read -r prevdatevar<${prevupload}
 echo Writing .tsv files to ${PATHNAME} with affix ${fileaffix}.
-( echo 'SELECT * FROM devices' | sudo mariadb -B hems ) > ${PATHNAME}devices${fileaffix}.tsv
-( echo 'SELECT * FROM energy' | sudo mariadb -B hems ) > ${PATHNAME}energy${fileaffix}.tsv
-( echo 'SELECT * FROM temperature' | sudo mariadb -B hems ) > ${PATHNAME}temperature${fileaffix}.tsv
+( echo "SELECT * FROM devices WHERE time > '${prevupload}'" | sudo mariadb -B hems ) > ${PATHNAME}devices${fileaffix}.tsv
+( echo "SELECT * FROM energy WHERE time > '${prevupload}'" | sudo mariadb -B hems ) > ${PATHNAME}energy${fileaffix}.tsv
+( echo "SELECT * FROM temperature WHERE time > '${prevupload}'" | sudo mariadb -B hems ) > ${PATHNAME}temperature${fileaffix}.tsv
+echo ${datetime} > ${preupload}
