bc2f3c4dddcd5f1006a534e54b5556b378835d4a
William Plucknett <william.plucknett@uky.edu>
2020-11-06 19:38:43 -0500

    Tried some different things than cat. none of them worked. back to cat

--- home-server/communications/sd.sh
+++ home-server/communications/sd.sh
@@ -2,11 +2,13 @@
 #Takes the ID number as first cla $1
 ./connectsd.sh $1
 sleep 5
-echo 1 $(date '+%Y-%m-%d %H:%M:%S') > SmartDevice$1.txt
+echo $1 $(date '+%Y-%m-%d %H:%M:%S') > SmartDevice$1.txt
 sudo echo a >> /dev/rfcomm0 2>&1
 sleep 1
 sudo cat /dev/rfcomm0 >> SmartDevice$1.txt 2>&1
-sleep 15
+#sudo tee SmartDevice$1.txt < /dev/rfcomm0
+#cp /dev/rfcomm0 SmartDevice$1.txt 
+#sleep 15
 echo Disconnecting...
 bash disconnect.sh 
 echo Writing to database...
