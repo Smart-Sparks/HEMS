0198cd67fa99c6d5990da4edd9d412b6946d1a10
William Plucknett <william.plucknett@uky.edu>
2020-11-08 13:26:45 -0500

    data files

--- home-server/communications/sd.sh
+++ home-server/communications/sd.sh
@@ -1,11 +1,13 @@
 
 #Takes the ID number as first cla $1
-./connectsd.sh $1
+#sudo echo a > ~/HEMS/home-server/communications/testy.txt
+~/HEMS/home-server/communications/connectsd.sh $1
 sleep 5
-echo $1 $(date '+%Y-%m-%d %H:%M:%S') > SmartDevice$1.txt
-sudo echo a >> /dev/rfcomm0 2>&1
+echo $1 $(date '+%Y-%m-%d %H:%M:%S') > ~/HEMS/home-server/communications/SmartDevice$1.txt
+sudo echo a >> /dev/rfcomm0 
 sleep 1
-sudo cat /dev/rfcomm0 >> SmartDevice$1.txt 2>&1
+sudo cat /dev/rfcomm0 >> ~/HEMS/home-server/communications/SmartDevice$1.txt 2>&1
+
 #sudo tee SmartDevice$1.txt < /dev/rfcomm0
 #cp /dev/rfcomm0 SmartDevice$1.txt 
 #sleep 15
