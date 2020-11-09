8a6fb79fb5984000a8d7bd5ed2a56b0d08f79306
William Plucknett <william.plucknett@uky.edu>
2020-11-09 16:38:10 -0500

    testing

--- home-server/communications/relayc.sh
+++ home-server/communications/relayc.sh
@@ -1,4 +1,4 @@
 #Disconnect power to the load 
-sudo bash ~/HEMS/home-server/communications/connectsd2.sh
-sleep 1
+#sudo bash ~/HEMS/home-server/communications/connectsd2.sh
+#sleep 1
 sudo echo c >> /dev/rfcomm0 2>&1
